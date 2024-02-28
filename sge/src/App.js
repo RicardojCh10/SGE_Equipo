d9a8c8e8f7ad5d8ad969a412b67e5f18e911a2e6
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2024-02-27 19:10:07 -0500

    Creación del Maquetado

---
+++ sge/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="py-4">
+          <h1 className="text-3xl font-bold text-center">Inicio</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
